d05eb5dc457f611c8dacc86f1efe0990400fe43f
codyvogt <cody.vogt@caltechsurveys.com>
2019-10-06 09:50:44 -0600

    Initial Commit

---
+++ Persistence/DataContext.cs
@@ -0,0 +1,17 @@
+﻿using Domain;
+using Microsoft.EntityFrameworkCore;
+using System;
+
+
+namespace Persistence
+{
+    public class DataContext : DbContext
+    {
+        public DataContext(DbContextOptions options) : base(options)
+        {
+            
+        }
+        public DbSet<Activity> Values { get; set; }
+        public DbSet<Activity> Activities { get; set; }
+    }
+}
